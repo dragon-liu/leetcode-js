f92d9aebd2e7c3205aba8de0b76d330102e0d8ed
dragon-liu <liuzhichao@hust.edu.cn>
2022-01-01 23:55:25 +0800

    剑指 Offer 30. 包含min函数的栈

---
+++ 剑指Offer/剑指 Offer 30. 包含min函数的栈/solve.js
@@ -0,0 +1,47 @@
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function () {
+  this.stack = [];
+  this.minStack = [Infinity]; //用于跟第一个入栈元素比较压入最小值
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function (x) {
+  this.stack.push(x);
+  this.minStack.push(Math.min(this.minStack[this.minStack.length - 1], x)); //压入辅助栈顶元素和当前元素间的最小值
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.stack.pop();
+  this.minStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.min = function () {
+  return this.minStack[this.minStack.length - 1];
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.min()
+ */
