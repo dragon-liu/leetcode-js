6384b39b49589403e5765d98d239819881a0ff79
dragon-liu <liuzhichao@hust.edu.cn>
2022-01-03 00:56:28 +0800

    剑指 Offer 24. 反转链表

---
+++ 剑指Offer/剑指 Offer 24. 反转链表/solve.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// 迭代解法
+// var reverseList = function(head) {
+//     let prev = null;
+//     let curr = head;
+//     while(curr){
+//         const next = curr.next;
+//         curr.next = prev;
+//         prev = curr;
+//         curr = next;
+//     }
+//     return prev;
+// };
+
+// 递归解法
+var reverseList = function (head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+  const newHead = reverseList(head.next);
+  head.next.next = head;
+  head.next = null;
+  return newHead;
+};
